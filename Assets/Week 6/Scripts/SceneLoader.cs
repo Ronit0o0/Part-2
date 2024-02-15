869e92b8ead16814219a95c36d8df2def5f1ec29
RONITSLAPTOP\ronbo <rathroni@sheridancollege.ca>
2024-02-15 14:35:01 -0500

    Week6 Task 2 Completed

---
+++ Assets/Week 6/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class SceneLoader : MonoBehaviour
+{ 
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+    
+   
+
+ 
+}
