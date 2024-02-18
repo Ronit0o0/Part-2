a06b31eb7397b66c45184d4fa6664d401f2320cb
RONITSLAPTOP\ronbo <rathroni@sheridancollege.ca>
2024-02-18 16:02:36 -0500

    Made starting menu button work and created temporary charcter and scnee that takes that switches scene with a door in the gameplay scene.

---
+++ Assets/Assignment/Scripts/SceneTransition.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public void ToGameScene() //This function will make it so the start buttons goes to the gameplay scene
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
